7485407bd88d42190649a6b87b28eab1bd7ba6c3
tymoschuk <tymoschuk@A34COMP8>
2019-02-07 17:43:45 +0200

    Add project files.

---
+++ Products/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Products.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        Context ctx=new Context();
+        public ActionResult ProductsTable()
+        {
+            
+            return View(ctx.Products);
+
+        }
+
+        public ActionResult LinkPage(string id)
+        {
+
+            return Redirect(ctx.Products.Where(v => v.Name == id).First().Link);
+
+        }
+
+    }
+}
